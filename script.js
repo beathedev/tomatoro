1138649995ef97b226643a344b1ee035e19ea1b7
beathedev <beatrizfrancisconi@souunisuam.com.br>
2021-10-27 18:30:30 -0300

    Countdown funcionando

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-const tempoHTML = document.querySelector('.tempo')
-let tempoTotal = 1500;
-let tempoRestante = 1500;
-let pausa = 300;
+const go = document.querySelector('.go')
+const pause = document.querySelector('.pause')
+const tempo = document.querySelector('.tempo')
+var tempoEmMinutos = 25;
+var expiracao = new Date(new Date().getTime() + tempoEmMinutos * 60000);
 
 
-clockTimer = setInterval(() => {
-    tempoRestante--;
-}, 1000);
+
+function callback() {
+    // Função a ser executada a cada 1 segundo
+    faltam = expiracao - new Date();
+    if (faltam <= 0) {
+        window.clearInerval(contador);
+        console.log("Prazo expirado");
+    }
+    minutos = Math.floor(faltam / 60000);
+    segundos = faltam % 60000;
+    tempoRestante = `${minutos}:${segundos.toString().substring(0,2)}`;
+    tempo.innerHTML = tempoRestante;
+}
+
+// Variável com o intervalo do setInterval()
+function intervalo() {
+    var intervalo = setInterval(callback, 1000)
+}
+go.addEventListener('click', intervalo)
+
+
+// Parando o intervalo
+pause.addEventListener('click', function() {
+    tempo.innerHTML = 'Pausado'
+    clearInterval(intervalo);
+
+})
